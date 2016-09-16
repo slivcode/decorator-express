c3e799aa061d82d04e7ee1b951ee49bd729db4c9
sliversniper <sliv.code@gmail.com>
2016-09-17 01:22:48 +0800

    init commit

---
+++ src/decorator/method.ts
@@ -0,0 +1,6 @@
+import { createMethodDecorator } from '../util/method-decorator'
+export let GET = createMethodDecorator('get')
+export let POST = createMethodDecorator('post')
+export let DELETE = createMethodDecorator('delete')
+export let PATCH = createMethodDecorator('patch')
+export let PUT = createMethodDecorator('put')
---
+++ src/decorator/middleware.ts
@@ -0,0 +1,19 @@
+import { assignDecorator, initDecorator } from '../util/init-decoration'
+export let USE = (...middlewares) => (a1, a2?, a3?) => {
+
+  // flatten the array for random array middleware usage
+  middlewares = middlewares.reduce((pr, ne) => pr.concat(ne), [])
+
+  switch (typeof a2) {
+    case 'string':
+      initDecorator(a1)
+      assignDecorator(a1, {route: {[a2]: {middlewares}}})
+      return
+    default:
+      initDecorator(a1)
+      assignDecorator(a1, {option: {middlewares}})
+      return
+  }
+
+
+}
